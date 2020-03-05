a7b474b0345e2317aa0e869cab44aceb74a12d00
Julien Boulen <julien.boulen@univ-rennes2.fr>
2020-03-05 12:44:33 +0100

    feat(test): ajoute l'intégration à Travis CI
    - ajoute un readme
    - ajoute une licence
    - ajoute un fichier editorconfig
    - complète le phpdoc
    - corrige le coding style

---
+++ README.md
@@ -0,0 +1,32 @@
+# block_apsolu_course
+
+[![Build Status](https://travis-ci.org/apsolu/block_apsolu_course.svg?branch=master)](https://travis-ci.org/apsolu/block_apsolu_course)
+[![Coverage Status](https://coveralls.io/repos/github/apsolu/block_apsolu_course/badge.svg?branch=master)](https://coveralls.io/github/apsolu/block_apsolu_course?branch=master)
+
+## Description
+
+Ce bloc permet d'afficher dans un cours donné :
+- pour un étudiant, ses propres présences
+- pour un enseignant, le total des présences par session de son cours
+
+Ce bloc donne également à un enseignant des raccourcis pour accéder aux pages de gestion des inscriptions et de prise des présences.
+
+
+## Installation
+
+```bash
+cd /your/moodle/path
+git clone https://github.com/apsolu/block_apsolu_course blocks/apsolu_course
+php admin/cli/upgrade.php
+```
+
+
+## Reporting security issues
+
+We take security seriously. If you discover a security issue, please bring it
+to their attention right away!
+
+Please **DO NOT** file a public issue, instead send your report privately to
+[foss-security@univ-rennes2.fr](mailto:foss-security@univ-rennes2.fr).
+
+Security reports are greatly appreciated and we will publicly thank you for it.
--- block_apsolu_course.php
+++ block_apsolu_course.php
@@ -15,13 +15,21 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Handles displaying the upcoming events block.
+ * Bloc affichant dans un cours un résumé des présences aux enseignants et les présences de l'étudiant.
  *
  * @package    block_apsolu_course
  * @copyright  2016 Université Rennes 2 <dsi-contact@univ-rennes2.fr>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Classe représentant le bloc block_apsolu_course.
+ *
+ * @copyright 2016 Université Rennes 2 <dsi-contact@univ-rennes2.fr>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 class block_apsolu_course extends block_base {
 
     /**
@@ -65,7 +73,7 @@ class block_apsolu_course extends block_base {
      * @return stdClass the content
      */
     public function get_content() {
-        global $CFG, $DB, $OUTPUT, $PAGE, $USER;
+        global $CFG, $OUTPUT;
 
         if ($this->content !== null) {
             return $this->content;
@@ -81,116 +89,144 @@ class block_apsolu_course extends block_base {
 
         if (has_capability('moodle/course:update', $context)) {
             // This is a teacher.
-            $sessions = array();
-            $count_sessions = 0;
-
-            $sql = "SELECT sessions.*, COUNT(presences.id) AS presences".
-                " FROM {apsolu_attendance_sessions} sessions".
-                " LEFT JOIN {apsolu_attendance_presences} presences ON sessions.id = presences.sessionid AND presences.statusid IN (1,2)".
-                " WHERE sessions.courseid = :courseid".
-                " AND sessions.sessiontime < :time".
-                " GROUP BY sessions.id".
-                " ORDER BY sessions.sessiontime";
-            $params = array(
-                'courseid' => $context->instanceid,
-                'time' => (time() + 8 * 7 * 24 * 60 * 60),
-                );
-            foreach ($DB->get_records_sql($sql, $params) as $session) {
-                $session->str_date = userdate($session->sessiontime, '%d %b %y');
-                $sessions[] = $session;
-                $count_sessions++;
-            }
-
-            $enrols = array();
-            $count_enrols = 0;
-            $params = array(
-                'enrol' => 'select',
-                'status' => 0,
-                'courseid' => $this->page->course->id,
-                );
-
-            foreach ($DB->get_records('enrol', $params) as $enrol) {
-                if (empty($enrol->name) === true) {
-                    $enrol->name = 'Gérer mes étudiants';
-                }
-
-                $enrols[] = $enrol;
-                $count_enrols++;
-            }
-
-            // Template data.
-            $data = new stdClass();
-            $data->wwwroot = $CFG->wwwroot;
-            $data->courseid = $context->instanceid;
-            $data->sessions = $sessions;
-            $data->count_sessions = $count_sessions;
-            $data->enrols = $enrols;
-            if ($count_enrols !== 0) {
-                $data->enrolid = $enrols[0]->id;
-            }
-            $data->count_enrols = $count_enrols;
-
+            $data = $this->get_teacher_content($context);
             $template = 'block_apsolu_course/teacher_block';
         } else {
             // This is a student.
-            $sessions = array();
-            $count_sessions = 0;
-
-            $sql = "SELECT sessions.*, status.name, status.code".
-                " FROM {apsolu_attendance_sessions} sessions".
-                " LEFT JOIN {apsolu_attendance_presences} presences ON sessions.id = presences.sessionid".
-                " LEFT JOIN {apsolu_attendance_statuses} status ON status.id = presences.statusid".
-                " WHERE sessions.courseid = :courseid".
-                " AND sessions.sessiontime < :time".
-                " AND (presences.studentid = :userid OR presences.studentid IS NULL)".
-                " ORDER BY sessions.sessiontime";
-            $params = array(
-                'courseid' => $context->instanceid,
-                'time' => (time() + 8 * 7 * 24 * 60 * 60),
-                'userid' => $USER->id,
-                );
-            foreach ($DB->get_records_sql($sql, $params) as $session) {
-                $session->str_date = userdate($session->sessiontime, '%d %b %y');
-
-                switch ($session->name) {
-                    case 'present':
-                        $session->str_status = get_string($session->code, 'local_apsolu');
-                        $session->css_status = 'text-success';
-                        break;
-                    case 'late':
-                        $session->str_status = get_string($session->code, 'local_apsolu');
-                        $session->css_status = 'text-warning';
-                        break;
-                    case 'excused':
-                        $session->str_status = get_string($session->code, 'local_apsolu');
-                        $session->css_status = 'text-info';
-                        break;
-                    case 'absent':
-                        $session->str_status = get_string($session->code, 'local_apsolu');
-                        $session->css_status = 'text-danger';
-                        break;
-                    default:
-                        $session->str_status = get_string('attendance_undefined', 'local_apsolu');
-                        $session->css_status = 'text-left';
-                }
-
-                $sessions[] = $session;
-                $count_sessions++;
-            }
-
-            // Template data.
-            $data = new stdClass();
-            $data->wwwroot = $CFG->wwwroot;
-            $data->courseid = $context->instanceid;
-            $data->sessions = $sessions;
-            $data->count_sessions = $count_sessions;
-
+            $data = $this->get_student_content($context);
             $template = 'block_apsolu_course/student_block';
         }
+
+        $data->wwwroot = $CFG->wwwroot;
+        $data->courseid = $context->instanceid;
 
         // Display template.
         $this->content->text = $OUTPUT->render_from_template($template, $data);
 
         return $this->content;
+    }
+
+    /**
+     * Return the content of this block for a student.
+     *
+     * @param stdClass $context Contexte dans lequel le bloc est utilisé.
+     *
+     * @return stdClass the content
+     */
+    private function get_student_content(stdClass $context) {
+        global $DB, $USER;
+
+        $sessions = array();
+        $countsessions = 0;
+
+        $sql = "SELECT sessions.*, status.name, status.code".
+            " FROM {apsolu_attendance_sessions} sessions".
+            " LEFT JOIN {apsolu_attendance_presences} presences ON sessions.id = presences.sessionid".
+            " LEFT JOIN {apsolu_attendance_statuses} status ON status.id = presences.statusid".
+            " WHERE sessions.courseid = :courseid".
+            " AND sessions.sessiontime < :time".
+            " AND (presences.studentid = :userid OR presences.studentid IS NULL)".
+            " ORDER BY sessions.sessiontime";
+        $params = array(
+            'courseid' => $context->instanceid,
+            'time' => (time() + 8 * 7 * 24 * 60 * 60),
+            'userid' => $USER->id,
+            );
+        foreach ($DB->get_records_sql($sql, $params) as $session) {
+            $session->str_date = userdate($session->sessiontime, '%d %b %y');
+
+            switch ($session->name) {
+                case 'present':
+                    $session->str_status = get_string($session->code, 'local_apsolu');
+                    $session->css_status = 'text-success';
+                    break;
+                case 'late':
+                    $session->str_status = get_string($session->code, 'local_apsolu');
+                    $session->css_status = 'text-warning';
+                    break;
+                case 'excused':
+                    $session->str_status = get_string($session->code, 'local_apsolu');
+                    $session->css_status = 'text-info';
+                    break;
+                case 'absent':
+                    $session->str_status = get_string($session->code, 'local_apsolu');
+                    $session->css_status = 'text-danger';
+                    break;
+                default:
+                    $session->str_status = get_string('attendance_undefined', 'local_apsolu');
+                    $session->css_status = 'text-left';
+            }
+
+            $sessions[] = $session;
+            $countsessions++;
+        }
+
+        // Template data.
+        $data = new stdClass();
+        $data->courseid = $context->instanceid;
+        $data->sessions = $sessions;
+        $data->count_sessions = $countsessions;
+
+        return $data;
+    }
+
+    /**
+     * Return the content of this block for a teacher.
+     *
+     * @param stdClass $context Contexte dans lequel le bloc est utilisé.
+     *
+     * @return stdClass the content
+     */
+    private function get_teacher_content(stdClass $context) {
+        global $DB;
+
+        $sessions = array();
+        $countsessions = 0;
+
+        $sql = "SELECT sessions.*, COUNT(presences.id) AS presences".
+            " FROM {apsolu_attendance_sessions} sessions".
+            " LEFT JOIN {apsolu_attendance_presences} presences ON sessions.id = presences.sessionid AND presences.statusid IN (1,2)".
+            " WHERE sessions.courseid = :courseid".
+            " AND sessions.sessiontime < :time".
+            " GROUP BY sessions.id".
+            " ORDER BY sessions.sessiontime";
+        $params = array(
+            'courseid' => $context->instanceid,
+            'time' => (time() + 8 * 7 * 24 * 60 * 60),
+            );
+        foreach ($DB->get_records_sql($sql, $params) as $session) {
+            $session->str_date = userdate($session->sessiontime, '%d %b %y');
+            $sessions[] = $session;
+            $countsessions++;
+        }
+
+        $enrols = array();
+        $countenrols = 0;
+        $params = array(
+            'enrol' => 'select',
+            'status' => 0,
+            'courseid' => $this->page->course->id,
+            );
+
+        foreach ($DB->get_records('enrol', $params) as $enrol) {
+            if (empty($enrol->name) === true) {
+                $enrol->name = 'Gérer mes étudiants';
+            }
+
+            $enrols[] = $enrol;
+            $countenrols++;
+        }
+
+        // Template data.
+        $data = new stdClass();
+        $data->sessions = $sessions;
+        $data->count_sessions = $countsessions;
+        $data->enrols = $enrols;
+        if ($countenrols !== 0) {
+            $data->enrolid = $enrols[0]->id;
+        }
+        $data->count_enrols = $countenrols;
+
+        return $data;
     }
 }
